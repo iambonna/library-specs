8ec1262d44a4119c53fcb1ca930ec50901ac0115
Bonna Choi <bchoi@thoughtworks.com>
2013-11-21 14:55:44 -0800

    bonna - addding tests to see if there is a book im looking looking for exists and tell me which library has that book

--- spec/search_catalog_spec.rb
+++ spec/search_catalog_spec.rb
@@ -8,10 +8,7 @@ describe 'Searching Catalog' do
     ce2 = CatalogEntry.new('book2', 'Oakland library')
     ls.catalog = [ce1, ce2]
 
-    #ls.find_book_by_title('book3')
-    #ls.does_library_have_this_book?('book1').should == true
-    #ls.does_library_have_this_book?('book1').should == true
-    #byebug
+    ls.does_library_have_this_book?('book1').should == true
     ls.does_library_have_this_book?('book3').should == false
   end
 
@@ -19,9 +16,13 @@ describe 'Searching Catalog' do
     ls = LibrarySystem.new
     ce1 = CatalogEntry.new('book1', 'SF library')
     ce2 = CatalogEntry.new('book2', 'Oakland library')
-    ls.catalog = [ce1, ce2]
-    ls.find_library_with_book('book1')
+    ls.catalog = [ ce2, ce1]
 
-   ls.find_library_with_book('book1').should == 'SF library'
+    ls.find_library_with_book('book1').should == 'SF library'
+
+    ls.find_book_by_title('book3').should == false
+    ls.find_book_by_title('book2').should == ce2
+    ls.find_book_by_title('book1').should == ce1
+    ls.find_library_with_book('book2').should == 'Oakland library'
   end
 end
--- src/library_system.rb
+++ src/library_system.rb
@@ -9,21 +9,26 @@ class LibrarySystem
       if catalog_entry.book_title == book_title
         return catalog_entry
       end
-      return false
     end
+    return false
   end
 
   def find_library_with_book(book_title)
-    if find_book_by_title(book_title)
-      @catalog.each do |catalog_entry|
-      end
-      library_name = @catalog[0].library_name
+    if find_book_by_title(book_title) != nil
+      book_im_looking_for = find_book_by_title(book_title)
+      return book_im_looking_for.library_name
     end
-    return library_name
+
+end
+
+def does_library_have_this_book?(book_title)
+  @catalog.each do |catalog_entry|
+    if catalog_entry.book_title == book_title
+      return true
+    end
+    return false
   end
 
-  def does_library_have_this_book?(book_title)
-    return find_book_by_title(book_title)
-  end
+end
 
 end
