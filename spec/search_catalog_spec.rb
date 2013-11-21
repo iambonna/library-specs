1101cb7f90035c753ee57d84dc5ef7e449ab5c8b
Bonna Choi <bchoi@thoughtworks.com>
2013-11-21 14:57:42 -0800

    bonna/robin/arnie - removing some assertions as they were added for debugging

--- spec/search_catalog_spec.rb
+++ spec/search_catalog_spec.rb
@@ -16,13 +16,9 @@ describe 'Searching Catalog' do
     ls = LibrarySystem.new
     ce1 = CatalogEntry.new('book1', 'SF library')
     ce2 = CatalogEntry.new('book2', 'Oakland library')
-    ls.catalog = [ ce2, ce1]
+    ls.catalog = [ce2, ce1]
 
     ls.find_library_with_book('book1').should == 'SF library'
-
-    ls.find_book_by_title('book3').should == false
-    ls.find_book_by_title('book2').should == ce2
-    ls.find_book_by_title('book1').should == ce1
     ls.find_library_with_book('book2').should == 'Oakland library'
   end
 end
