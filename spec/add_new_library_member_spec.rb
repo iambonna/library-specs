5d7dd0e3ffc1f2db83c36499e52d0b7149735a6a
Bonna Choi <bchoi@thoughtworks.com>
2013-12-03 12:12:29 -0800

    bonna - user can be a member at multiple libraries

--- spec/add_new_library_member_spec.rb
+++ spec/add_new_library_member_spec.rb
@@ -6,7 +6,7 @@ require 'byebug'
 
 describe 'adding new library member' do
   it 'should let a user to add new library member' do
-    sf_library = Library.new("SF library")
+    sf_library = Library.new('SF library')
     sf_library.members.size.should == 0
     sf_library.add_new_member_to_library('Bonna Choi').should == 'Bonna Choi is now a member of a SF library.'
     sf_library.members.size.should == 1
@@ -14,9 +14,22 @@ describe 'adding new library member' do
     sf_library.members.size.should == 2
   end
 
-  #it 'should not allow to add new member that already exist in the library' do
-  #  ls = LibrarySystem.new
-  #  ls.add_new_member_to_library('SF library', 'Bonna Choi').should == 'Bonna Choi is already a member of a SF library.'
-  #end
+  it 'should not allow to add new member that already exist in the library' do
+    sf_library = Library.new('SF library')
+    sf_library.add_new_member_to_library('Bonna Choi')
+    sf_library.members.size.should == 1
+    sf_library.add_new_member_to_library('Bonna Choi').should == 'Bonna Choi is already a member of a SF library.'
+    sf_library.members.size.should == 1
+  end
+
+  it 'should let a user to a library membership from multiple libraries' do
+    sf_library = Library.new('SF library')
+    chicago_library = Library.new('Chicago library')
+    sf_library.add_new_member_to_library('Bonna Choi').should == 'Bonna Choi is now a member of a SF library.'
+    sf_library.members.size.should == 1
+    chicago_library.add_new_member_to_library('Bonna Choi').should == 'Bonna Choi is now a member of a Chicago library.'
+    chicago_library.members.size.should == 1
+  end
+
 
 end
--- src/library.rb
+++ src/library.rb
@@ -10,10 +10,13 @@ class Library
   end
 
   def add_new_member_to_library(person_name)
+    if @members.include? person_name
+      "Bonna Choi is already a member of a SF library."
+    else
 
     @members << person_name
     "#{person_name} is now a member of a #{library_name}."
-
+    end
   end
 
 end
